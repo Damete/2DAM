c4b148b065326385ad539c10d95e1cf52e904b48
Damete <dfebrer@nigul.cide.es>
2020-02-05 17:41:18 +0100

    comit message

---
+++ Servicios/Sockets/BufferedStreamCliente.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+
+public class BufferedStreamCliente{
+    public static void main(String[] args) {
+        try{
+            Socket socketCliente = new Socket();
+            InetSocketAddress direccion = new InetSocketAddress("localhost", 5555);
+            socketCliente.connect(direccion);
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+            DataOutputStream outToServer = new DataOutputStream(socketCliente.getOutputStream());
+            String mensaje = "Mensaje del clinete";
+            outToServer.writeBytes(mensaje);
+            socketCliente.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
