72d7391b633c4445b3a59f6cefdb60a07b5db79e
Damete <dfebrer@nigul.cide.es>
2020-02-11 17:52:03 +0100

    fixed errorhandling

--- Servicios/Sockets/TCPServer.java
+++ Servicios/Sockets/TCPServer.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 public class TCPServer{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         String clientSentence;
         String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(44014);
