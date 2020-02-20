a98198f4fe3e3f75c21647788cc9343c45ea26b7
Damete <dfebrer@nigul.cide.es>
2020-02-20 15:27:45 +0100

    Servidor y cliente UDP terminados

--- Servicios/Sockets/EjerciciosComunicacionRed/E3/SPU03E01e3_2client_Damia_Febrer.java
+++ Servicios/Sockets/EjerciciosComunicacionRed/E3/SPU03E01e3_2client_Damia_Febrer.java
@@ -3,21 +3,19 @@ import java.io.*;
 
 public class SPU03E01e3_2client_Damia_Febrer{
     public static void main(String[] args) throws Exception{
-        DatagramSocket ds = new DatagramSocket();  
-        String enviar = "Pepe";
-
-        while(true){              
-            InetAddress addr = InetAddress.getByName("localhost");  
+        DatagramSocket ds = new DatagramSocket(5555);  
+        String enviar = "Uep";
+ 
+        InetAddress addr = InetAddress.getByName("localhost");  
             
-            DatagramPacket dp = new DatagramPacket(enviar.getBytes(), enviar.length(), addr, 44014);  
-            ds.send(dp);  
-            System.out.println("Mensaje enviado");
+        DatagramPacket dp = new DatagramPacket(enviar.getBytes(), enviar.length(), addr, 44014);  
+        ds.send(dp);  
+        System.out.println("Mensaje enviado: " + enviar);
 
-            byte[] recibido = new byte[1024];  
-            DatagramPacket dpr = new DatagramPacket(recibido, 1024);  
-            ds.receive(dp);  
-            String delServer = new String(dp.getData(), 0, dp.getLength());  
-            System.out.println(delServer);
-        }        
+        byte[] recibido = new byte[1024];  
+        DatagramPacket dpr = new DatagramPacket(recibido, 1024);  
+        ds.receive(dpr);  
+        String delServer = new String(dpr.getData(), 0, dpr.getLength());  
+        System.out.println(delServer);
     }
 }
