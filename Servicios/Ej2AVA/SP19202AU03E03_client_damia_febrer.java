44b048c86a0bee40bd72e41fe718b715a13bce83
Damete <dfebrer@nigul.cide.es>
2020-03-01 17:21:28 +0100

    E finito

---
+++ Servicios/Ej2AVA/SP19202AU03E03_client_damia_febrer.java
@@ -0,0 +1,114 @@
+package Servicios.Ej2AVA;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class SP19202AU03E03_client_damia_febrer{
+    public static void main(String[] args) {      
+
+        try{
+            Socket cliente = new Socket();
+            InetSocketAddress addr = new InetSocketAddress("localhost", 44014);
+            cliente.connect(addr);
+
+            InputStream is = cliente.getInputStream();
+            OutputStream os = cliente.getOutputStream();
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Bienvenido a la compra online de entradas, ¿que acción desea realizar? \n" +
+                                "1. Mostrar número de entradas libres \n" +
+                                "2. Vender entradas \n" +
+                                "3. Salir \n" +
+                                "=>");
+    
+            String opcion = sc.nextLine();
+            byte [] paraServer = "Hola".getBytes();
+    
+            switch(opcion){
+                case"1":
+                paraServer = opcion.getBytes();
+                os.write(paraServer);
+
+                byte [] pregunta = new byte[200];
+                is.read(pregunta);
+                String preguntaZona = new String(pregunta);
+                System.out.println(preguntaZona);
+
+                String respuestaZona = sc.nextLine();
+                os.write(respuestaZona.getBytes());
+    
+                byte [] numeroEntradas = new byte [5];
+                is.read(numeroEntradas);
+                String entradasDisponibles = formatearNumero(numeroEntradas);
+                System.out.println("Para la zona " + respuestaZona + " quedan " + entradasDisponibles + " entradas disponibles");
+                break;
+    
+                case"2":
+                paraServer = opcion.getBytes();
+                os.write(paraServer);
+
+                byte [] delServer = new byte [200];
+                is.read(delServer);
+                String opcionesZona = new String(delServer);
+                System.out.println(opcionesZona);
+
+                String zona = sc.nextLine();
+                if(zona.equals("platea") || zona.equals("Platea")){
+                    paraServer = zona.getBytes();
+                    os.write(paraServer);
+                }
+                else if(zona.equals("anfiteatro") || zona.equals("Anfiteatro")){
+                    paraServer = zona.getBytes();
+                    os.write(paraServer);
+                }
+                else if(zona.equals("VIP") || zona.equals("vip")){
+                    paraServer = zona.getBytes();
+                    os.write(paraServer);
+                }
+                else{
+                    System.out.println("La opción introducida es invalida");
+                }
+
+                byte [] cantidad = new byte[200];
+                is.read(cantidad);
+                String preguntaCantidad = new String(cantidad);
+                System.out.println("\n" + preguntaCantidad);
+
+                String entradas = sc.nextLine();
+                paraServer = entradas.getBytes();
+                os.write(paraServer);
+
+                byte [] resulOperacion = new byte[200];
+                is.read(resulOperacion);
+                String resultado = new String(resulOperacion);
+                System.out.println("\n" + resultado);
+                break;
+    
+                case"3":
+                break;
+    
+                default:
+                System.out.println("El valor proporcionado es incorrecto, por favor vuelva a intentarlo");
+            }           
+
+
+        } catch(IOException e)        {
+            e.printStackTrace();
+        }
+    }
+
+    public static String formatearNumero(byte[] numero){
+        String sinFormatear = new String(numero);
+        String formateado = "";
+
+        for(int i = 0; i < sinFormatear.length(); i++){
+            if(sinFormatear.charAt(i) != 00){
+                formateado += sinFormatear.charAt(i);
+            }
+        }
+
+        return formateado;
+    }
+}
---
+++ Servicios/Ej2AVA/SP19202AU03E03_servidor_damia_febrer.java
@@ -0,0 +1,128 @@
+package Servicios.Ej2AVA;
+
+import java.io.*;
+import java.net.*;
+
+public class SP19202AU03E03_servidor_damia_febrer{
+    public static void main(String[] args) {
+        try{
+            ServerSocket servidor = new ServerSocket(44014);
+            Zona platea = new Zona(1000);
+            Zona anfiteatro = new Zona(200);
+            Zona VIP = new Zona(25);
+
+
+            while(true){
+                Socket nuevo = servidor.accept();
+
+                InputStream is = nuevo.getInputStream();
+                OutputStream os = nuevo.getOutputStream();
+
+                byte [] delCliente = new byte[1];
+                is.read(delCliente);
+                String opcion = new String(delCliente);
+
+                if(opcion.contains("1")){
+                    String preguntaZona = "¿Para que zona desea ver la cantidad restante de entradas?, Platea, Anfiteatro o VIP?";
+                    byte [] preguntarZona = preguntaZona.getBytes();
+                    os.write(preguntarZona);
+
+                    byte [] zona = new byte [10];
+                    is.read(zona);
+                    String zonaElegida = new String(zona);
+
+                    if(zonaElegida.contains("Platea") || zonaElegida.contains("platea")){
+                        int entradas = platea.getEntradasParaVender();
+                        String resultado = String.valueOf(entradas);
+                        os.write(resultado.getBytes());
+                    }
+                    else if(zonaElegida.contains("Anfiteatro") || zonaElegida.contains("anfiteatro")){
+                        int entradas = anfiteatro.getEntradasParaVender();
+                        String resultado = String.valueOf(entradas);
+                        os.write(resultado.getBytes());
+                    }
+                    else if(zonaElegida.contains("VIP") || zonaElegida.contains("vip")){
+                        int entradas = VIP.getEntradasParaVender();
+                        String resultado = String.valueOf(entradas);
+                        os.write(resultado.getBytes());
+                    }
+                }
+                else if(opcion.contains("2")){
+                    String preguntaZona = "¿Para que zona desea comprar las entradas, Platea, Anfiteatro o VIP?";
+                    byte [] preguntarZona = preguntaZona.getBytes();
+                    os.write(preguntarZona);
+
+                    byte [] zona = new byte [10];
+                    is.read(zona);
+                    String zonaElegida = new String(zona);
+
+                    String preguntaCantidad = "¿Quantas entradas desea comprar?";
+                    byte [] preguntarCantidad = preguntaCantidad.getBytes();
+                    os.write(preguntarCantidad);
+
+                    byte [] cantidad = new byte[10];
+                    is.read(cantidad);
+                    String help = new String(cantidad);
+                    int cantidadEspecificada = formatearNumero(help);
+
+                    if(zonaElegida.contains("Platea") || zonaElegida.contains("platea")){
+                        String resultado = platea.vender(cantidadEspecificada);
+                        os.write(resultado.getBytes());
+                    }
+                    else if(zonaElegida.contains("Anfiteatro") || zonaElegida.contains("anfiteatro")){
+                        String resultado = anfiteatro.vender(cantidadEspecificada);
+                        os.write(resultado.getBytes());
+                    }
+                    else if(zonaElegida.contains("VIP") || zonaElegida.contains("vip")){
+                        String resultado = VIP.vender(cantidadEspecificada);
+                        os.write(resultado.getBytes());
+                    }
+                }
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static int formatearNumero(String numero){
+        String formateado = "";
+
+        for(int i = 0; i < numero.length(); i++){
+            if(numero.charAt(i) != 00){
+                formateado += numero.charAt(i);
+            }
+        }
+
+        int listo = Integer.parseInt(formateado);
+
+        return listo;
+    }
+
+    public static class Zona{
+        private int entradasParaVender;
+        
+        public Zona(int n){
+            entradasParaVender = n;
+        }
+
+        public int getEntradasParaVender(){
+            return entradasParaVender;
+        }
+
+        public String vender(int n){
+            String respuesta ="";
+
+            if(this.entradasParaVender == 0){
+                respuesta = ("Lo sentimos, las entradas para la zona que ha elegido están agotadas");
+            } else if(this.entradasParaVender < n){
+                respuesta = ("Solo quedan " + entradasParaVender + " disponibles para la zona que ha seleccionado");
+            }
+            if(this.entradasParaVender >= n){
+                entradasParaVender -= n;
+                respuesta = ("Aquí tiene sus " + n + " entradas, gracias por su compra");
+            }
+
+            return respuesta;
+        }
+    }
+}
